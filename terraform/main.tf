226ea5a7814f054cb1dcb6771eaa8eb110418ae4
Gabriel Villarroel <gabriel.villarroel@mews.com>
2025-11-25 19:11:56 +0100

    Fix terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   features {}
+  subscription_id = "21327150-ec4c-4db3-ae03-7d5cef898b9c"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -8,70 +9,67 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "${var.resource_group_name}sa"
+  name                     = "storageaccount"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_cosmosdb_account" "cosmos" {
-  name                = "${var.resource_group_name}cosmos"
+resource "azurerm_cosmosdb_account" "cosmos-account" {
+  name                = "${var.resource_group_name}-cosmos-db"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   offer_type          = "Standard"
   kind                = "GlobalDocumentDB"
   consistency_policy {
-    consistency_level = "Session"
+    consistency_level       = "Session"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
   }
   geo_location {
     location          = azurerm_resource_group.rg.location
     failover_priority = 0
   }
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
 }
 
 resource "azurerm_cosmosdb_sql_database" "database" {
   name                = "PhoneAgendaDb"
   resource_group_name = azurerm_resource_group.rg.name
-  account_name        = azurerm_cosmosdb_account.cosmos.name
+  account_name        = azurerm_cosmosdb_account.cosmos-account.name
 }
 
 resource "azurerm_cosmosdb_sql_container" "container" {
   name                = "Contacts"
   resource_group_name = azurerm_resource_group.rg.name
-  account_name        = azurerm_cosmosdb_account.cosmos.name
+  account_name        = azurerm_cosmosdb_account.cosmos-account.name
   database_name       = azurerm_cosmosdb_sql_database.database.name
-  partition_key_path  = "/id"
+  partition_key_paths   = ["/id"]
   throughput          = 400
 }
 
-resource "azurerm_function_app" "function" {
-  name                       = "${var.resource_group_name}-func"
-  resource_group_name        = azurerm_resource_group.rg.name
-  location                   = azurerm_resource_group.rg.location
+resource "azurerm_linux_function_app" "function" {
+  name                        = "${var.resource_group_name}-func"
+  resource_group_name         = azurerm_resource_group.rg.name
+  location                    = azurerm_resource_group.rg.location
   storage_account_name        = azurerm_storage_account.storage.name
   storage_account_access_key  = azurerm_storage_account.storage.primary_access_key
-  os_type                    = "Linux"
-  version                    = "~4"
-  functions_extension_version = "~4"
-  app_service_plan_id         = azurerm_app_service_plan.plan.id
+  service_plan_id             = azurerm_service_plan.plan.id
 
-  site_config {
-    linux_fx_version = "DOTNET-ISOLATED|8.0"
-    application_settings = {
-      "COSMOS_CONNECTION_STRING" = azurerm_cosmosdb_account.cosmos.connection_strings[0]
-    }
+  site_config {}
+
+  app_settings = {
+    "COSMOS_CONNECTION_STRING" = azurerm_cosmosdb_account.cosmos-account.primary_sql_connection_string
   }
 }
 
-resource "azurerm_app_service_plan" "plan" {
+resource "azurerm_service_plan" "plan" {
   name                = "${var.resource_group_name}-plan"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  kind                = "FunctionApp"
-  reserved            = true
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
+  sku_name		      = "Y1"
+  os_type             = "Linux"
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "function_app_name" {
-  value = azurerm_function_app.function.name
+  value = azurerm_linux_function_app.function.name
 }
 
-output "cosmos_connection_string" {
-  value = azurerm_cosmosdb_account.cosmos.connection_strings[0]
-}
+/* output "cosmos_connection_string" {
+  value = azurerm_cosmosdb_account.cosmos-account.primary_sql_connection_string
+} */
